225067d121734ba316758dc1b573fb5bf0df9e44
Stefan Foulis <stefan@foulis.ch>
2012-12-11 13:51:20 +0100

    WIP untested adaption of picture plugin save to new api

--- djangocms_text_ckeditor/picture_save.py
+++ djangocms_text_ckeditor/picture_save.py
@@ -3,7 +3,7 @@ from django.conf import settings
 import os
 import random
 
-def create_picture_plugin(image_type, file_ending, image, parent_plugin, width, height):
+def create_picture_plugin(filename, image, parent_plugin, **kwargs):
     from cms.plugins.picture.models import Picture
     pic = Picture()
     pic.placeholder = parent_plugin.placeholder
@@ -11,17 +11,12 @@ def create_picture_plugin(image_type, file_ending, image, parent_plugin, width, 
     pic.position = CMSPlugin.objects.filter(parent=parent_plugin).count()
     pic.language = parent_plugin.language
     pic.plugin_type = 'PicturePlugin'
-    if width or height:
-        image.rezise(width=width, height=height)
-    name = ""
-    while len(name) < 10:
-        name += random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-    path = pic.get_media_path(name + "." + file_ending)
+    path = pic.get_media_path(filename)
     full_path = os.path.join(settings.MEDIA_ROOT, path)
     if not os.path.exists(os.path.dirname(full_path)):
         os.makedirs(os.path.dirname(full_path))
     pic.image = path
     f = open(full_path, "wb")
-    image.save(f, image_type)
+    image.write(f)
     pic.save()
     return pic

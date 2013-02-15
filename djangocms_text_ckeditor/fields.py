b679e453167f9cc72825166588c02980f6cca36d
Aleš Kocjančič <alesdotio@gmail.com>
2013-02-15 14:08:44 +0100

    clean the fields value

--- djangocms_text_ckeditor/fields.py
+++ djangocms_text_ckeditor/fields.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.admin import widgets as admin_widgets
+from djangocms_text_ckeditor.html import clean_html
 from djangocms_text_ckeditor.widgets import TextEditorWidget
 
 
@@ -13,3 +14,7 @@ class HTMLField(models.TextField):
             defaults['widget'] = TextEditorWidget
 
         return super(HTMLField, self).formfield(**defaults)
+
+    def clean(self, value, model_instance):
+        value = super(HTMLField, self).clean(value, model_instance)
+        return clean_html(value, full=False)

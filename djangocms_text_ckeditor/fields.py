d2949c9c1ffb0afba424b1871bcc44933c681315
Aleš Kocjančič <alesdotio@gmail.com>
2013-03-22 16:24:38 +0100

    added south introspection rules to the field

--- djangocms_text_ckeditor/fields.py
+++ djangocms_text_ckeditor/fields.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.admin import widgets as admin_widgets
 from djangocms_text_ckeditor.html import clean_html
 from djangocms_text_ckeditor.widgets import TextEditorWidget
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ['^djangocms_text_ckeditor\.fields\.HTMLField'])
+except ImportError:
+    pass
 
 
 class HTMLField(models.TextField):

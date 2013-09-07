e3442e931fd123efd592c7672860f4611037ee7b
Charlie Denton <charlie@meshy.co.uk>
2013-09-07 01:11:53 +0100

    Mimic signature of deprecated truncate_words.

--- djangocms_text_ckeditor/compat.py
+++ djangocms_text_ckeditor/compat.py
@@ -3,5 +3,6 @@ try:
 except ImportError:
     from django.utils.text import truncate_words # Django < 1.4
 else:
-    def truncate_words(value, length):
-        return Truncator(value).words(length)
+    def truncate_words(s, num, end_text='...'):
+        end_text = ' {0}'.format(end_text) if end_text else ''
+        return Truncator(s).words(num, truncate=end_text)

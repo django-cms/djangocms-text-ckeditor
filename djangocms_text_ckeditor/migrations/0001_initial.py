bd594ccd7ccc91a34cb8139faa9d45f1d1f225b7
Marco Paolini <marco@credra.com>
2013-11-16 22:35:50 +0100

    Make migration dependant on `cms`.

--- djangocms_text_ckeditor/migrations/0001_initial.py
+++ djangocms_text_ckeditor/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('cms', '0001_initial.py'),
+    )
+
     def forwards(self, orm):
         # Adding model 'Text'
         db.create_table('cmsplugin_text', (

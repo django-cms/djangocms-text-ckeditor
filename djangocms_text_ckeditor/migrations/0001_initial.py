9452b89d6a558e8e5120a7a6641f5b00ed09c8d9
Marco Paolini <marco@credra.com>
2013-11-16 22:35:50 +0100

    Make it dependent on `cms:0001_initial` as advised by @yakky.

--- djangocms_text_ckeditor/migrations/0001_initial.py
+++ djangocms_text_ckeditor/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.db import models
 class Migration(SchemaMigration):
 
     depends_on = (
-        ('cms', '0012_publisher.py'),
+        ('cms', '0001_initial.py'),
     )
 
     def forwards(self, orm):

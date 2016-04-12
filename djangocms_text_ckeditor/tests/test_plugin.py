60e984d89bc1f4359c6eb584a4eaf6483d9c8b6f
Paulo <commonzenpython@gmail.com>
2016-04-12 18:00:17 -0400

    refactor plugin tests

--- djangocms_text_ckeditor/tests/test_plugin.py
+++ djangocms_text_ckeditor/tests/test_plugin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import re
 
 from cms.api import create_page
 from cms.models import CMSPlugin, Page
@@ -19,20 +20,10 @@ class PluginActionsTestCase(CMSTestCase, BaseTestCase):
     def get_post_request(self, data):
         return self.get_request(post_data=data)
 
-    def get_plugin_id_from_response(self, response, path):
-        # Expects response to be a JSON response
-        # with a structure like so:
-        # {'path.1': {
-        #   'path.2': '/en/admin/app/example1/edit-plugin/3/'}
-        # }
-        # path is used to access nested keys
-
-        data = json.loads(response.content.decode('utf-8'))
-        keys = path.split('.')
-
-        for key in keys:
-            data = data[key]
-        return data.split('/')[-2]
+    def get_plugin_id_from_response(self, response):
+        # Ideal case, this looks like:
+        # /en/admin/cms/page/edit-plugin/1/
+        return re.findall('\d+', response.url)[0]
 
     def test_add_and_edit_plugin(self):
         """
@@ -43,27 +34,38 @@ class PluginActionsTestCase(CMSTestCase, BaseTestCase):
 
         native_placeholder_admin = self.get_page_admin()
 
-        request = self.get_post_request({
+        request = self.get_request()
+        request.GET = {
             'plugin_type': 'TextPlugin',
             'placeholder_id': simple_placeholder.pk,
             'plugin_language': 'en',
             'plugin_parent': '',
-        })
+        }
         response = native_placeholder_admin.add_plugin(request)
 
-        self.assertEqual(response.status_code, 200)
+        text_plugin_pk = self.get_plugin_id_from_response(response)
 
-        # Point to the newly created text plugin
-        text_plugin_pk = self.get_plugin_id_from_response(
-            response,
-            path='url',
-        )
+        self.assertIn('?delete-on-cancel', response.url)
+        self.assertEqual(response.status_code, 302)
 
         # Assert "ghost" plugin has been created
         self.assertObjectExist(CMSPlugin.objects.all(), pk=text_plugin_pk)
 
+        cms_plugin = CMSPlugin.objects.get(pk=text_plugin_pk)
+        text_plugin_class = cms_plugin.get_plugin_class_instance()
+
         # Assert "real" plugin has not been created yet
         self.assertObjectDoesNotExist(Text.objects.all(), pk=text_plugin_pk)
+
+        with self.login_user_context(self.get_superuser()):
+            request = self.get_request()
+            cancel_token = text_plugin_class.get_cancel_token(request, cms_plugin)
+            response = self.client.get(response.url)
+
+            self.assertEqual(response.status_code, 200)
+
+            # Assert cancel token is present
+            self.assertContains(response, cancel_token)
 
         request = self.get_post_request({'body': "Hello world"})
         response = native_placeholder_admin.edit_plugin(request, text_plugin_pk)
@@ -78,7 +80,6 @@ class PluginActionsTestCase(CMSTestCase, BaseTestCase):
         # Assert the text was correctly saved
         self.assertEqual(text_plugin.body, "Hello world")
 
-
     def test_add_and_cancel_plugin(self):
         """
         Test that you can add a text plugin
@@ -88,29 +89,33 @@ class PluginActionsTestCase(CMSTestCase, BaseTestCase):
 
         native_placeholder_admin = self.get_page_admin()
 
-        request = self.get_post_request({
+        request = self.get_request()
+        request.GET = {
             'plugin_type': 'TextPlugin',
             'placeholder_id': simple_placeholder.pk,
             'plugin_language': 'en',
             'plugin_parent': '',
-        })
+        }
+
         response = native_placeholder_admin.add_plugin(request)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
         # Point to the newly created text plugin
-        text_plugin_pk = self.get_plugin_id_from_response(
-            response,
-            path='url',
-        )
+        text_plugin_pk = self.get_plugin_id_from_response(response)
+        cms_plugin = CMSPlugin.objects.get(pk=text_plugin_pk)
+        text_plugin_class = cms_plugin.get_plugin_class_instance()
 
         # Assert "ghost" plugin has been created
         self.assertObjectExist(CMSPlugin.objects.all(), pk=text_plugin_pk)
 
-        request = self.get_post_request({'body': "Hello world", '_cancel': True})
-        response = native_placeholder_admin.edit_plugin(request, text_plugin_pk)
-
-        self.assertEqual(response.status_code, 200)
+        with self.login_user_context(self.get_superuser()):
+            request = self.get_request()
+            cancel_token = text_plugin_class.get_cancel_token(request, cms_plugin)
+            data = {'plugin': text_plugin_pk, 'token': cancel_token}
+            request = self.get_post_request(data)
+            response = text_plugin_class.delete_on_cancel(request)
+            self.assertEqual(response.status_code, 204)
 
         # Assert "ghost" plugin has been removed
         self.assertObjectDoesNotExist(CMSPlugin.objects.all(), pk=text_plugin_pk)

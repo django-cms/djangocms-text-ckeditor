e83585f19d5649d5c30dde41fdece081819abbd8
Paulo <commonzenpython@gmail.com>
2016-04-16 12:02:08 -0400

    adds htmlform field

--- djangocms_text_ckeditor/fields.py
+++ djangocms_text_ckeditor/fields.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.forms.fields import CharField
 from django.contrib.admin import widgets as admin_widgets
 from django.db import models
 from django.utils.safestring import mark_safe
@@ -13,14 +14,33 @@ except ImportError:
     pass
 
 
+class HTMLFormField(CharField):
+
+    widget = TextEditorWidget
+
+    def __init__(self, *args, **kwargs):
+        conf = kwargs.pop('configuration', None)
+
+        if conf:
+            widget = TextEditorWidget(configuration=conf)
+        else:
+            widget = None
+        kwargs.setdefault('widget', widget)
+        super(HTMLFormField, self).__init__(*args, **kwargs)
+
+    def clean(self, value):
+        value = super(HTMLFormField, self).clean(value)
+        return clean_html(value, full=False)
+
+
 class HTMLField(models.TextField):
     configuration = None
 
     def __init__(self, *args, **kwargs):
-        # This allow widget configuration customization from the model definition
-        if kwargs.get('configuration', False):
-            self.configuration = kwargs['configuration']
-            del(kwargs['configuration'])
+        # This allows widget configuration customization
+        # from the model definition
+        if kwargs.get('configuration', None):
+            self.configuration = kwargs.pop('configuration')
         super(HTMLField, self).__init__(*args, **kwargs)
 
     def from_db_value(self, value, expression, connection, context):
@@ -39,18 +59,17 @@ class HTMLField(models.TextField):
 
     def formfield(self, **kwargs):
         if self.configuration:
-            text_editor_widget = TextEditorWidget(configuration=self.configuration)
+            widget = TextEditorWidget(configuration=self.configuration)
         else:
-            text_editor_widget = TextEditorWidget
-        defaults = {'widget': text_editor_widget}
+            widget = TextEditorWidget
+
+        defaults = {
+            'form_class': HTMLFormField,
+            'widget': TextEditorWidget,
+        }
         defaults.update(kwargs)
 
         # override the admin widget
         if defaults['widget'] == admin_widgets.AdminTextareaWidget:
-            defaults['widget'] = text_editor_widget
-
+            defaults['widget'] = widget
         return super(HTMLField, self).formfield(**defaults)
-
-    def clean(self, value, model_instance):
-        value = super(HTMLField, self).clean(value, model_instance)
-        return clean_html(value, full=False)

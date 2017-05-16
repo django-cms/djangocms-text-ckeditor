2a9ec2c39a097ca12a240e31402cbf3041002368
Paulo <commonzenpython@gmail.com>
2017-05-16 05:55:37 -0400

    Removed references to cms_content_renderer

--- djangocms_text_ckeditor/templatetags/djangocms_text_ckeditor_tags.py
+++ djangocms_text_ckeditor/templatetags/djangocms_text_ckeditor_tags.py
@@ -1,12 +1,19 @@
 from django import template
 
+from cms.toolbar.utils import get_toolbar_from_request
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def render_plugin_preview(context, plugin):
-    content_renderer = context.get('cms_content_renderer')
+    request = context['request']
+    toolbar = get_toolbar_from_request(request)
+
+    try:
+        content_renderer = toolbar.content_renderer
+    except AttributeError:
+        content_renderer = None
 
     if content_renderer:
         content = content_renderer.render_plugin(

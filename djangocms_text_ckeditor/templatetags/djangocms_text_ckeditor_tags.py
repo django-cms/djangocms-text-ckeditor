92ed2ab9d0d10d0e9f4b55604d9fb42749b2d152
Paulo <commonzenpython@gmail.com>
2017-06-29 07:15:58 -0400

    Added 3.5 tests

--- djangocms_text_ckeditor/templatetags/djangocms_text_ckeditor_tags.py
+++ djangocms_text_ckeditor/templatetags/djangocms_text_ckeditor_tags.py
@@ -1,16 +1,14 @@
 from django import template
 
-from cms.toolbar.utils import get_toolbar_from_request
-
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def render_plugin_preview(context, plugin):
     request = context['request']
-    toolbar = get_toolbar_from_request(request)
 
     try:
+        toolbar = request.toolbar
         content_renderer = toolbar.content_renderer
     except AttributeError:
         content_renderer = None

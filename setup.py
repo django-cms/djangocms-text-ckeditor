64359e83c99f509381f6f0258e1b17e71368b0f5
Tim Graham <timograham@gmail.com>
2017-11-05 06:33:08 -0500

    Added testing for Django 1.10 & 1.11 (#448)

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ CLASSIFIERS = [
     'Framework :: Django',
     'Framework :: Django :: 1.8',
     'Framework :: Django :: 1.9',
+    'Framework :: Django :: 1.10',
+    'Framework :: Django :: 1.11',
 ]
 
 setup(

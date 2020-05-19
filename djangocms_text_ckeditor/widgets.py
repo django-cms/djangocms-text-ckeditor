58736c4f5b0184ad50f549f14d163052a061a3aa
Jacob Rief <jacob.rief@gmail.com>
2020-05-19 22:34:49 +0200

    Rebundle ckeditor for version 4.14.0

--- djangocms_text_ckeditor/widgets.py
+++ djangocms_text_ckeditor/widgets.py
@@ -16,7 +16,7 @@ from . import settings as text_settings
 
 
 # this path is changed automatically whenever you run `gulp bundle`
-PATH_TO_JS = 'djangocms_text_ckeditor/js/dist/bundle-45a646fecc.cms.ckeditor.min.js'
+PATH_TO_JS = 'djangocms_text_ckeditor/js/dist/bundle-1418376b7e.cms.ckeditor.min.js'
 
 
 class TextEditorWidget(forms.Textarea):

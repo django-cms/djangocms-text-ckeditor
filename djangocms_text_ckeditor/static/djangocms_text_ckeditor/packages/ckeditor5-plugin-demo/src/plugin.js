79d4d5641f3f7f26847ea13fe2838c832c4d603b
ns11 <alexander.cudreasov@gmail.com>
2020-12-17 16:29:28 +0100

    remove unsed resources

--- djangocms_text_ckeditor/static/djangocms_text_ckeditor/packages/ckeditor5-plugin-demo/src/plugin.js
+++
@@ -1,9 +0,0 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import { toWidget, viewToModelPositionOutsideModelElement } from '@ckeditor/ckeditor5-widget/src/utils';
-import Widget from '@ckeditor/ckeditor5-widget/src/widget';
-import Command from '@ckeditor/ckeditor5-core/src/command';
-
-import { addListToDropdown, createDropdown } from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
-import Collection from '@ckeditor/ckeditor5-utils/src/collection';
-import Model from '@ckeditor/ckeditor5-ui/src/model';
-

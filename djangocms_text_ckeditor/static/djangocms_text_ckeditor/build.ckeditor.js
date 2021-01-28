95f3f464390a42afe4564650ad599fc65cf091ea
ns11 <alexander.cudreasov@gmail.com>
2021-01-28 16:20:04 +0100

    dll integration, add yarn for building the ck inside the djangocms addon

--- djangocms_text_ckeditor/static/djangocms_text_ckeditor/build.ckeditor.js
+++ djangocms_text_ckeditor/static/djangocms_text_ckeditor/build.ckeditor.js
@@ -1,11 +1,5 @@
 // TODO: 	window.CKEDITOR_BASEPATH = document.querySelector('[data-ckeditor-basepath]').getAttribute('data-ckeditor-basepath');
 
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import ClassicEditor from 'build/ckeditor5-dll';
 
 export default ClassicEditor;

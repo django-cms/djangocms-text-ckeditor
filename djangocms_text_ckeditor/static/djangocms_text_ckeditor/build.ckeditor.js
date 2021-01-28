8b96b870f23119a62d30caba88a7aeae62d84acb
ns11 <alexander.cudreasov@gmail.com>
2021-01-28 17:32:59 +0100

    change import path for esbuild

--- djangocms_text_ckeditor/static/djangocms_text_ckeditor/build.ckeditor.js
+++ djangocms_text_ckeditor/static/djangocms_text_ckeditor/build.ckeditor.js
@@ -1,5 +1,5 @@
 // TODO: 	window.CKEDITOR_BASEPATH = document.querySelector('[data-ckeditor-basepath]').getAttribute('data-ckeditor-basepath');
 
-import ClassicEditor from 'build/ckeditor5-dll';
+import ClassicEditor from './build/ckeditor5-dll';
 
 export default ClassicEditor;

01766fc6dff6faecbd64f942acd1551c7c36fa3a
Fabian Braun <fsbraun@gmx.de>
2022-03-27 22:16:57 +0200

    Feature:	Inline editing

---
+++ djangocms_text_ckeditor/cms_toolbars.py
@@ -0,0 +1,13 @@
+from cms.cms_toolbars import BasicToolbar, CMSToolbar
+from cms.toolbar_pool import toolbar_pool
+
+
+@toolbar_pool.register
+class InlineEditingToolbar(CMSToolbar):
+    class Media:
+        ...
+        # css = {"all": ("djangocms_text_ckeditor/css/cms.ckeditor.inline.css",)}
+        js = (
+            "djangocms_text_ckeditor/ckeditor/ckeditor.js",
+            "djangocms_text_ckeditor/js/cms.inline-editor.js",
+        )

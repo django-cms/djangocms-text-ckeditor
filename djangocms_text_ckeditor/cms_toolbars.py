10bfceb81e70824af861053af93298622d42eac9
Fabian Braun <fsbraun@gmx.de>
2022-03-28 08:48:06 +0200

    Fix:		Inline editing js only loaded in edit mode.

--- djangocms_text_ckeditor/cms_toolbars.py
+++ djangocms_text_ckeditor/cms_toolbars.py
@@ -1,13 +1,18 @@
 from cms.cms_toolbars import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
-
+from django import forms
 
 @toolbar_pool.register
 class InlineEditingToolbar(CMSToolbar):
-    class Media:
-        ...
-        # css = {"all": ("djangocms_text_ckeditor/css/cms.ckeditor.inline.css",)}
-        js = (
-            "djangocms_text_ckeditor/ckeditor/ckeditor.js",
-            "djangocms_text_ckeditor/js/cms.inline-editor.js",
-        )
+    @property
+    def media(self):
+        if self.toolbar.edit_mode_active:
+            return forms.Media(
+                js = (
+                    "djangocms_text_ckeditor/ckeditor/ckeditor.js",
+                    "djangocms_text_ckeditor/js/cms.inline-editor.js",
+                )
+            )
+        return forms.Media()
+
+

ccb9e2eaf54d98b481667f0342813cc82af734fc
Fabian Braun <fsbraun@gmx.de>
2022-05-16 16:09:25 +0200

    Test:		Test edit mode

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -519,13 +519,13 @@ class PluginActionsTestCase(BaseTestCase):
             text_plugin = self.add_plugin_to_text(text_plugin, plugin)
 
         with self.login_user_context(self.get_superuser()):
-            response = self.client.get(simple_page.get_absolute_url() + "?inline_editing=1")
+            response = self.client.get(simple_page.get_absolute_url() + "?edit&inline_editing=1")
             print(response.content.decode("utf-8"))
             self.assertEqual(response.status_code, 200)
             self.assertContains(response, "<cms-plugin")
 
         with self.login_user_context(self.get_superuser()):
-            response = self.client.get(simple_page.get_absolute_url() + "?inline_editing=0")
+            response = self.client.get(simple_page.get_absolute_url() + "?edit&inline_editing=0")
             print(response.content.decode("utf-8"))
             self.assertEqual(response.status_code, 200)
             self.SimpleTestCase.assertNotContains(response, "<cms-plugin")

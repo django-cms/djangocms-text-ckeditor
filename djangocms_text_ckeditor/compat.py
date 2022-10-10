6e5536e4fa0827d77056c7f5389dfca9bf461874
Fabian Braun <fsbraun@gmx.de>
2022-10-10 11:07:11 +0200

    Remove unused compat.py

--- djangocms_text_ckeditor/compat.py
+++
@@ -1,6 +0,0 @@
-def get_page_placeholders(page, language=None):
-    try:
-        # cms 3.6 compat
-        return page.get_placeholders()
-    except TypeError:
-        return page.get_placeholders(language)
